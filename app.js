0e202f5314ae92f497f74da9e837080ed5f7bbd3
akash <akpatra12345@gmail.com>
2022-12-28 19:13:09 +0530

    akash

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use('/api/v1/jobs',authenticatedUser,jobsRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 const start = async () =>{
     try{
